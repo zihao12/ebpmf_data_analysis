562d70e279ec7edfbefdadf012c994e45b85451a
zihao12 <wzihao12@gmail.com>
2020-05-14 22:45:51 -0500

    update script for experiment

--- script/fit_uci_BoW_ebpmf_bg.R
+++ script/fit_uci_BoW_ebpmf_bg.R
@@ -8,10 +8,11 @@ args = commandArgs(trailingOnly=TRUE)
 docname = args[1]
 K = as.integer(args[2])
 maxiter = as.integer(args[3])
+every = as.integer(args[4])
 
-
+version = "v0.3.9"
 datadir = "../data/uci_BoW"
-outdir = "../output/uci_BoW"
+outdir = sprintf("../output/uci_BoW/%s", version)
 filename = sprintf("docword.%s", docname)
 format = "txt"
 file_out = sprintf("%s/%s_ebpmf_bg_K%d_maxiter%d.Rds", 
@@ -21,15 +22,32 @@ Y = read_uci_bag_of_words(file= sprintf("%s/%s.%s",
 			    datadir,filename, format)) 
 
 
-print("fitting")
-runtime <- system.time(
-        fit <- ebpmf.alpha::ebpmf_bg(X = Y, K = K,
-				     pm_func = list(f = ebpm::ebpm_gamma_mixture, l = ebpm::ebpm_gamma_mixture),
-				     init = NULL,
-				     maxiter = maxiter, verbose  = TRUE)
-)
-runtime
-fit[["runtime"]] = runtime
-saveRDS(fit, file = file_out)
 
+T = round(maxiter/every)
+init = NULL
+ELBO = c()
+RUNTIME = c()
+
+start_time = proc.time()
+for(t in 1:T){
+	start_iter = 1 + (t-1)*every
+	end_iter = t*every
+	file_out = sprintf("%s/%s_ebpmf_bg_K%d_maxiter%d.Rds",
+       outdir,docname, K, end_iter)
+	print("##########################################")
+	print(sprintf("start fitting from %d iteration", start_iter))
+	fit <- ebpmf.alpha::ebpmf_bg(X = Y, K = K,
+															 pm_func = list(f = ebpm::ebpm_gamma_mixture, l = ebpm::ebpm_gamma_mixture),
+															 init = init,
+															 maxiter = every, verbose  = TRUE)
+	print(sprintf("finish fitting %d iteration", end_iter))
+	runtime = proc.time() - start_time
+	## update init, elbo
+	init = list(qg = fit$qg, l0 = fit$l0, f0 = fit$f0)
+	ELBO = c(ELBO, fit$ELBO)
+	## save file
+	fit[["ELBO"]] = ELBO
+	fit[["runtime"]] = runtime
+	saveRDS(fit, file = file_out)
+}
 print(sessionInfo())
--- script/fit_uci_BoW_ebpmf_bg.sh
+++ script/fit_uci_BoW_ebpmf_bg.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-Rscript --no-save --no-restore --verbose fit_uci_BoW_ebpmf_bg.R kos 2 10 > fit_kos_ebpmf_bg.Rout 2>&1
+Rscript --no-save --no-restore --verbose fit_uci_BoW_ebpmf_bg.R kos 20 5000 500 > ../output/uci_BoW/v0.3.9/fit_kos_ebpmf_bg_K20_maxiter_5000.Rout 2>&1
