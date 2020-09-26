74d10c79b4e5a3221ddfb7b6a36482f72b913a62
zihao12 <wzihao12@gmail.com>
2020-09-26 16:23:29 -0500

    update analysis and util

--- analysis/plot_topic_words.R
+++ analysis/plot_topic_words.R
@@ -5,21 +5,27 @@ library(gridExtra)
 source("../code/misc.R")
 source("../code/util.R")
 
-version = "v0.3.9"
-K = 100
+version_wbg = "v0.4.2"
+version_pmf = "v0.3.9"
 
-output_dir = sprintf("../output/uci_BoW/%s/", version)
-plot_file = sprintf("../docs/kos_K%d_%s_topic_words.pdf", K, version)
+K = 20
+
+output_dir_wbg = sprintf("../output/uci_BoW/%s/", version_wbg)
+output_dir_pmf = sprintf("../output/uci_BoW/%s/", version_pmf)
+
+plot_file = sprintf("../docs/kos_K%d_%s_topic_words.pdf", K, version_wbg)
 data_dir = "../data/uci_BoW/"
-model_name = sprintf("kos_ebpmf_bg_initLF50_K%d_maxiter2000.Rds", K)
+model_wbg_initLF_name = sprintf("kos_ebpmf_wbg_initLF50_K%d_maxiter2000.Rds", K)
+model_wbg_initL_name = sprintf("kos_ebpmf_wbg_initL50_K%d_maxiter2000.Rds", K)
 model_pmf_name = sprintf("kos_pmf_initLF50_K%d_maxiter2000.Rds", K)
 dict_name = "vocab.kos.txt"
 data_name = "docword.kos.txt"
 
 Y = read_uci_bag_of_words(file= sprintf("%s/%s",
                                         data_dir,data_name))
-model = readRDS(sprintf("%s/%s", output_dir, model_name))
-model_pmf = readRDS(sprintf("%s/%s", output_dir, model_pmf_name))
+model_wbg1 = readRDS(sprintf("%s/%s", output_dir_wbg, model_wbg_initLF_name))
+model_wbg2 = readRDS(sprintf("%s/%s", output_dir_wbg, model_wbg_initL_name))
+model_pmf = readRDS(sprintf("%s/%s", output_dir_pmf, model_pmf_name))
 dict = read.csv(sprintf("%s/%s", data_dir, dict_name), header = FALSE)[,1]
 dict = as.vector(dict)
 
@@ -27,17 +33,24 @@ dict = as.vector(dict)
 n = nrow(Y)
 p = ncol(Y)
 n_top_word = round(0.002 * p)
-L_pmf = model_pmf$L; F_pmf = model_pmf$F
-L_bg = model$l0 * model$qg$qls_mean; F_bg = model$f0 * model$qg$qfs_mean
-lf = poisson2multinom(L=L_bg,F=F_bg)
-lf_pmf = poisson2multinom(L = L_pmf,F = F_pmf)
+L_pmf = model_pmf$L
+F_pmf = model_pmf$F
+
+L_wbg1 = model_wbg1$l0 * model_wbg1$qg$qls_mean
+F_wbg1 = model_wbg1$f0 * model_wbg1$qg$qfs_mean
+
+L_wbg2 = model_wbg2$l0 * model_wbg2$qg$qls_mean
+F_wbg2 = model_wbg2$f0 * model_wbg2$qg$qfs_mean
 
 ## scale L, F so that colSums(F) = 1
 L_pmf = L_pmf %*% diag(colSums(F_pmf))
 F_pmf = F_pmf %*% diag(1/colSums(F_pmf))
 
-L_bg = L_bg %*% diag(colSums(F_bg))
-F_bg = F_bg %*% diag(1/colSums(F_bg))
+L_wbg1 = L_wbg1 %*% diag(colSums(F_wbg1))
+F_wbg1 = F_wbg1 %*% diag(1/colSums(F_wbg1))
+
+L_wbg2 = L_wbg2 %*% diag(colSums(F_wbg2))
+F_wbg2 = F_wbg2 %*% diag(1/colSums(F_wbg2))
 
 
 show_topic_util <- function(F, idx, main){
@@ -54,38 +67,22 @@ show_topic_util <- function(F, idx, main){
 
 pdf(plot_file, onefile = TRUE)
 for(k in 1:K){
-  #k = 1
-  ## show top words(bar_f_jk) in bar_f_jk
-  F = model$qg$qfs_mean
+  F = model_wbg1$qg$qfs_mean
   idx = order(F[,k], decreasing = TRUE)[1:n_top_word]
-  main = sprintf("bg topic %d", k)
+  main = sprintf("wbg_initLF topic %d", k)
   p1 = show_topic_util(F = F, idx = idx, main = main)
-  ## show top words(bar_f_jk) in f_jk
-  F = F_pmf
-  main = sprintf("pmf topic %d", k)
-  p2 = show_topic_util(F = F, idx = idx, main = main)
-  ## show top words(f_j0 * bar_f_jk) in bar_f_jk
-  F = F_bg
-  idx = order(F[,k], decreasing = TRUE)[1:n_top_word]
-  main = sprintf("bg topic %d", k)
-  p3 = show_topic_util(F = F, idx = idx, main = main)
-  ## show top words (pmf) in f_jk
-  F = F_pmf
-  idx = order(F[,k], decreasing = TRUE)[1:n_top_word]
-  main = sprintf("pmf topic %d", k)
-  p4 = show_topic_util(F = F, idx = idx, main = main)
-  ## show top words (pmf) in f_jk
-  F = lf$F
-  idx = order(F[,k], decreasing = TRUE)[1:n_top_word]
-  main = sprintf("bg topic %d", k)
-  p5 = show_topic_util(F = F, idx = idx, main = main)
-  ## show top words (pmf) in f_jk
-  F = lf_pmf$F
-  idx = order(F[,k], decreasing = TRUE)[1:n_top_word]
-  main = sprintf("pmf topic %d", k)
-  p6 = show_topic_util(F = F, idx = idx, main = main)
 
-  grid.arrange(grobs = list(p1, p2, p3, p4, p5, p6), ncol = 2)
+  F = model_wbg2$qg$qfs_mean
+  idx = order(F[,k], decreasing = TRUE)[1:n_top_word]
+  main = sprintf("wbg_initL topic %d", k)
+  p2 = show_topic_util(F = F, idx = idx, main = main)
+
+  F = F_pmf
+  idx = order(F[,k], decreasing = TRUE)[1:n_top_word]
+  main = sprintf("pmf topic %d", k)
+  p3 = show_topic_util(F = F, idx = idx, main = main)
+
+  grid.arrange(grobs = list(p1, p2, p3), ncol = 2)
 }
 
 dev.off()
--- code/util.R
+++ code/util.R
@@ -14,6 +14,13 @@ read_uci_bag_of_words <- function(filename){
 	return(out)
 }
 
+read_sla_bag_of_words <- function(filename){
+  data = read.table(file = filename, skip= 2, sep = " ",
+                    col.names= c("i", "j", "x"))
+  out = Matrix::sparseMatrix(i = data$i, j = data$j, x = data$x)
+  return(out)
+}
+
 ##################################################
 ################ Initialization ##################
 ##################################################
